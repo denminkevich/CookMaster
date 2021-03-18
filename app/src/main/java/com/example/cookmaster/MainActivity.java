370fbb79ac09e6a432fc0a64ee2524817dec3f9a
patrego <febol444@gmail.com>
2021-03-18 13:56:04 +0300

    add new activity(ShoppingListActivity) add imageButton on MainActivity and ShoppingListActivity add OnClick functions

--- app/src/main/java/com/example/cookmaster/MainActivity.java
+++ app/src/main/java/com/example/cookmaster/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.cookmaster;
 
 import android.os.Bundle;
-
+import android.content.Intent;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -12,13 +12,27 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+        private ImageButton ImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton = (ImageButton) findViewById(R.id.imageButtonFavorite);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openShoppingListActivity();
+            }
+        });
+    }
+    public void openShoppingListActivity(){
+        Intent intent = new Intent(this, ShoppingListActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/cookmaster/ShoppingListActivity.java
@@ -0,0 +1,31 @@
+package com.example.cookmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ShoppingListActivity extends AppCompatActivity {
+
+    private ImageButton ImageButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+
+        ImageButton = (ImageButton) findViewById(R.id.imageButtonMain3);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+}
+}
